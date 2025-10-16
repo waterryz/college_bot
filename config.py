744efae64b7457edc0066e5b54e0260610a716aa
waterryz <alexyss.waterry@icloud.com>
2025-10-16 17:45:35 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
+
+# === ТВОЙ TELEGRAM TOKEN ===
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # добавь в Render Environment
+
+# === ДОМЕН Render (после деплоя вставь сюда свой URL) ===
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://your-app.onrender.com/webhook")
+
+# === URL сайта колледжа ===
+COLLEGE_URL = "https://college.snation.kz"
 
