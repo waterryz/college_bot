129bf9570b4b32e86967534d12c023fa0f43ff21
waterryz <alexyss.waterry@icloud.com>
2025-10-16 19:42:22 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,38 +1,42 @@
 import sqlite3
 from cryptography.fernet import Fernet
-
 from config import ENCRYPTION_KEY
 
-f = Fernet(ENCRYPTION_KEY)
-
 def init_db():
-    conn = sqlite3.connect("students.db")
+    conn = sqlite3.connect("users.db")
     c = conn.cursor()
     c.execute("""
-    CREATE TABLE IF NOT EXISTS students (
-        user_id INTEGER PRIMARY KEY,
-        login TEXT,
-        password TEXT
-    )
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            login TEXT,
+            password TEXT
+        )
     """)
     conn.commit()
     conn.close()
 
 def save_credentials(user_id, login, password):
-    conn = sqlite3.connect("students.db")
+    cipher = Fernet(ENCRYPTION_KEY)
+    enc_login = cipher.encrypt(login.encode()).decode()
+    enc_password = cipher.encrypt(password.encode()).decode()
+
+    conn = sqlite3.connect("users.db")
     c = conn.cursor()
-    encrypted = f.encrypt(password.encode()).decode()
-    c.execute("REPLACE INTO students (user_id, login, password) VALUES (?, ?, ?)",
-              (user_id, login, encrypted))
+    c.execute("REPLACE INTO users (user_id, login, password) VALUES (?, ?, ?)", (user_id, enc_login, enc_password))
     conn.commit()
     conn.close()
 
 def get_credentials(user_id):
-    conn = sqlite3.connect("students.db")
+    cipher = Fernet(ENCRYPTION_KEY)
+
+    conn = sqlite3.connect("users.db")
     c = conn.cursor()
-    c.execute("SELECT login, password FROM students WHERE user_id=?", (user_id,))
+    c.execute("SELECT login, password FROM users WHERE user_id = ?", (user_id,))
     row = c.fetchone()
     conn.close()
+
     if row:
-        return row[0], f.decrypt(row[1].encode()).decode()
+        login = cipher.decrypt(row[0].encode()).decode()
+        password = cipher.decrypt(row[1].encode()).decode()
+        return login, password
     return None, None
