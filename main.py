ec54dd1e81aadaa95cf3c531e07506189f3b20d7
waterryz <alexyss.waterry@icloud.com>
2025-10-16 20:03:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-from fastapi import FastAPI, Request
-from aiogram import Bot, Dispatcher
 from db import init_db
-from config import BOT_TOKEN, WEBHOOK_URL
-from bot import router
+from bot import main
+import asyncio
 
-app = FastAPI()
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-dp.include_router(router)
-
-@app.on_event("startup")
-async def on_startup():
+if __name__ == "__main__":
     init_db()
-    await bot.set_webhook(WEBHOOK_URL)
-    print("✅ Webhook set successfully!")
-
-@app.post("/webhook")
-async def webhook(request: Request):
-    update = await request.json()
-    await dp.feed_raw_update(bot, update)
-    return {"ok": True}
-
-@app.get("/")
-async def root():
-    return {"status": "running"}
+    asyncio.run(main())
