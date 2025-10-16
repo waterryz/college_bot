02949e85ddf0c7686273f2f3069b2fe40a0a1ce6
waterryz <alexyss.waterry@icloud.com>
2025-10-16 20:30:37 +0500

    Update db.py

--- db.py
+++ db.py
@@ -14,14 +14,19 @@ def init_db():
     conn.commit()
     conn.close()
 
+
 def save_credentials(user_id, login, password):
     conn = sqlite3.connect("users.db", check_same_thread=False)
     c = conn.cursor()
     enc_login = fernet.encrypt(login.encode()).decode()
     enc_password = fernet.encrypt(password.encode()).decode()
-    c.execute("REPLACE INTO users (user_id, login, password) VALUES (?, ?, ?)", (user_id, enc_login, enc_password))
+    c.execute("""
+        REPLACE INTO users (user_id, login, password)
+        VALUES (?, ?, ?)
+    """, (user_id, enc_login, enc_password))
     conn.commit()
     conn.close()
+
 
 def get_credentials(user_id):
     conn = sqlite3.connect("users.db", check_same_thread=False)
@@ -29,6 +34,7 @@ def get_credentials(user_id):
     c.execute("SELECT login, password FROM users WHERE user_id = ?", (user_id,))
     row = c.fetchone()
     conn.close()
+
     if row:
         login = fernet.decrypt(row[0].encode()).decode()
         password = fernet.decrypt(row[1].encode()).decode()
